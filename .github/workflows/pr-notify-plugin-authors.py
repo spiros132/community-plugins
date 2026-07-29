9d28c16169525b2de09b092f4aa1716afe6716c8
Spyridon Siarapis <spiros.siarapis@gmail.com>
2026-07-29 13:06:58 +0200

    feat(workflow): Added workflow for notifying plugin authors in prs

---
+++ .github/workflows/pr-notify-plugin-authors.py
@@ -0,0 +1,79 @@
+import os
+import sys
+
+from github import Auth, Github, UnknownObjectException, BadCredentialsException
+
+PR_ID_LABEL = "- **Id:**"
+
+token = os.environ["GITHUB_TOKEN"]
+repo_name = os.environ["REPOSITORY"]
+pull_request_number = os.environ["PULL_REQUEST_NUMBER"]
+
+auth = Auth.Token(token)
+gh = Github(auth=auth)
+repo = gh.get_repo(repo_name)
+
+
+if pull_request_number.isdigit():
+    pull_request_number = int(pull_request_number)
+else:
+    print("Pull Request number is not numeric!")
+    sys.exit(1)
+
+pr = repo.get_pull(pull_request_number)
+body = pr.body
+lines = body.splitlines()
+
+pr_author = pr.user.login
+
+try:
+    id_index = lines.index(PR_ID_LABEL)
+except ValueError:
+    print(f"Pull Request '{pull_request_number}' had no id label. Not a plugin PR probably.")
+    sys.exit(0)
+
+id_line = lines[id_index]
+
+plugin = id_line.replace(PR_ID_LABEL, "").replace("`", "")
+plugin_split = plugin.split("/")
+
+if len(plugin_split) != 2:
+    print(f"Unknown format of plugin name, got {plugin}")
+    sys.exit(1)
+
+author_name = plugin_split[0]
+plugin_name = plugin_split[1]
+
+def is_same_author(author: str):
+    if author == pr_author:
+        print("The author and maintainer of the plugin are the same!")
+        sys.exit(0)
+
+is_same_author(author_name)
+
+author = ""
+try:
+    gh.get_user(author_name, lazy=False)
+    author = author_name
+except UnknownObjectException:
+    print(f"User '{author_name}' doesn't exist as a username in github, getting the first commits author!")
+
+    manifest_file = f"{plugin_name}/plugin.toml"
+
+    if os.path.exists(manifest_file):
+        file_commits = repo.get_commits(path=manifest_file).reversed
+        author = file_commits[0].author.login
+    else:
+        print(f"Plugin manifest doesn't exist, {manifest_file}")
+        sys.exit(1)
+except BadCredentialsException:
+    print("Invalid Github token")
+    sys.exit(1)
+
+if author:
+    pr.create_issue_comment(f"CC {author}")
+else:
+    print("Could not get the author.")
+    sys.exit(1)
+
+
